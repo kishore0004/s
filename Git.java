a5c3d5bfb41eccb44a999a252020b1eb4b87102d
kishore0004 <kishorekrishjan20@gmail.com>
2021-11-30 12:03:37 +0530

    first

---
+++ Git.java
@@ -0,0 +1,8 @@
+package org.add;
+
+public class Git {
+	public static void main(String[] args) {
+		System.out.println("kishore");
+	}
+
+}
---
+++ Git1.java
@@ -0,0 +1,7 @@
+package org.add;
+
+public class Git1 {
+	public static void main(String[] args) {
+		System.out.println("sdfghjk");
+	}
+}
